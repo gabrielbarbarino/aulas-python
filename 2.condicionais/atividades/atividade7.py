810294e33b4c11753b673f086f8eaa07454acfaa
gabrielbarbarino <112452901+gabrielbarbarino@users.noreply.github.com>
2024-04-11 19:55:36 +0000

    atualização

---
+++ 2.condicionais/atividades/atividade7.py
@@ -0,0 +1,9 @@
+vetor = []
+somaTotal = 0
+
+for i in range(0,5):
+    numero = int(input(f"Digite o {i + 1}º Numero: "))
+    vetor.append(numero)
+    somaTotal += numero
+
+print(f"A soma total foi: {numero}")
