92786379373f5e6b48cbbc153928bd82ca069f5c
gabrielbarbarino <112452901+gabrielbarbarino@users.noreply.github.com>
2024-04-23 17:36:13 +0000

    aaa

--- 2.condicionais/atividades/atividade15.py
+++ 2.condicionais/atividades/atividade15.py
@@ -1,7 +1,19 @@
 import os
 os.system("clear")
 
-while True:
+notas = []
+
+for i in range(6):
     numero = int(input("Digite um numero: "))
-    while numero % 2 == 0 or numero > 0:
+    
+    while numero < 0 or numero % 2 == 1:
+        print("Opção invalida!")
+        numero = int(input("Digite um numero: "))
         
+
+    notas.append(numero)
+
+os.system("clear")
+print("---------  resultados  ---------")
+for i in range(5, -1, -1):
+    print(f"{notas[i]}")
